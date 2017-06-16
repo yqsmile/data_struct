12b9b15eafa23cfa47d2ecfddd08ce700185719b
umint <urmylifeq@qq.com>
2017-06-16 17:20:07 +0800

    2ed

--- Link_list/Link_list.c
+++ Link_list/Link_list.c
@@ -20,18 +20,36 @@ void insert(Node *insert_node){
 }
 
 void remove_node(Node *p){
-
+  if(p==head){
+    head=head->next;
+  }
+  else{
+    if(!p->next){
+      p->pre->next=NULL;
+    }
+    else{
+      p->pre->next=p->next;
+      p->next->pre=p->pre;
+    }
+  }
 }
 int main(){
   char a[]="wqnmlgb";
   char b[]="ghjkl";
+  char c[]="g;;;;jkk";
 
   // int a=5;
   // int b=6;
   Node inode={a,NULL,NULL};
   Node inode2={b,NULL,NULL};
+  Node inode3={c,NULL,NULL};
   insert(&inode);
   insert(&inode2);
+  insert(&inode3);
+  for(Node *p=head;p;p=p->next){
+    printf("%s\n",p->data );
+  }
+  remove_node(&inode3);
   for(Node *p=head;p;p=p->next){
     printf("%s\n",p->data );
   }
